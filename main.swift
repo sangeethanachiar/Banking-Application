d790da0ca2e4124111fc5ab649bb94492fc45604
sangeethanachiar <53001534+sangeethanachiar@users.noreply.github.com>
2021-06-16 09:14:19 +0530

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,11 +1,39 @@
 //
 //  main.swift
-//  Banking2
+//  Banking App
 //
-//  Created by Sangeetha Nachiar on 16/06/21.
+//  Created by Sangeetha Nachiar on 14/06/21.
 //
+
 
 import Foundation
 
-print("Hello, World!")
+protocol authenticate {
+    func validate(index:Int,password :Int)->Bool
+}
 
+
+class Manager {
+    func getChoice(){
+    print("1.Bank\n2.Online Banking\n3.ATM ")
+        let ch = Int(readLine()!)
+        switch(ch){
+        case 1:
+         var ban = Bank()
+          getChoice()
+            getChoice()
+        case 2:
+            var  obj = OnlineBanking()
+            obj.getChoice1()
+            getChoice()
+        case 3:
+           var atm =  ATM()
+            atm.getScreen()
+            getChoice()
+        default :
+            print("Invalid")
+        }
+}
+}
+var obj = Manager()
+obj.getChoice()
